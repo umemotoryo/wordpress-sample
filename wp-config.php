51317ccf35adedb68b238ca7cd0cfbf399b2697c
umemotoryo <umemoto0722@gmail.com>
2018-06-01 18:30:08 +0900

    fix mysql character

--- wp-config.php
+++ wp-config.php
@@ -3,7 +3,7 @@ define('DB_NAME',     getenv('WP_DB_NAME'));
 define('DB_USER',     getenv('WP_DB_USER'));
 define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
 define('DB_HOST',     getenv('WP_DB_HOST'));
-define('DB_CHARSET',  'utf8md4');
+define('DB_CHARSET',  'utf8mb4');
 define('DB_COLLATE',  '');
 
 /**#@+
