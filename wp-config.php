2215109f601a7f0d98f2d2ec688329f384eb4aee
umemotoryo <umemoto0722@gmail.com>
2018-06-01 18:15:00 +0900

    first commit

---
+++ wp-config.php
@@ -0,0 +1,38 @@
+<?php
+define('DB_NAME',     getenv('WP_DB_NAME'));
+define('DB_USER',     getenv('WP_DB_USER'));
+define('DB_PASSWORD', getenv('WP_DB_PASSWORD'));
+define('DB_HOST',     getenv('WP_DB_HOST'));
+define('DB_CHARSET',  'utf8md4');
+define('DB_COLLATE',  '');
+
+/**#@+
+ * 認証用ユニークキー
+ *
+ * それぞれを異なるユニーク (一意) な文字列に変更してください。
+ * {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org の秘密鍵サービス} で自動生成することもできます。
+ * 後でいつでも変更して、既存のすべての cookie を無効にできます。これにより、すべてのユーザーを強制的に再ログインさせることになります。
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',       'put your unique phrase here');
+
+/**#@-*/
+
+$table_prefix  = 'wp_';
+
+define('WP_DEBUG', false);
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
